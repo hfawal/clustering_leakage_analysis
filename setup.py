0feb87bb1b1215a4542c2b529673224a15566d26
hfawal <hfawal@uw.edu>
2024-08-07 18:17:10 +0200

    Edited version for first PyPI upload.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='leakyblobs',
-    version='0.1.1',    
+    version='0.1.0',    
     description='Clustering leakage analysis library.',
     url='https://git.equancy.cloud/equancy/data-intelligence/clustering_leakage_analysis/-/tree/1_initial_dev_HF',
     author='Hady Fawal',
@@ -20,7 +20,7 @@ setup(
                       ],
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',     
         'Programming Language :: Python :: 3.11',
